49ddcc24ed363b904aab9f9f2914becbda80564f
Ashraf-DIU <ashrafbialam17@ggmail.com>
2025-01-15 00:02:10 +0600

    adding list method

---
+++ list_metod.py
@@ -0,0 +1,20 @@
+marks = [7 , 4, 12, 8]
+print(marks)
+
+marks.append(10)
+print(marks)
+
+marks.sort()
+print(marks)
+
+marks.sort(reverse=True)
+print(marks)
+
+marks.reverse()
+print(marks)
+
+marks.insert(1, 700)
+print(marks)
+
+marks.remove(10)
+print(marks)
