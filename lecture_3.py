497eb827c4b30a1ecee30b2a3183c6737d7d4cec
Ashraf-DIU <ashrafbialam17@ggmail.com>
2025-01-14 23:31:24 +0600

    Adding First Lecture

---
+++ lecture_3.py
@@ -0,0 +1,24 @@
+# marks1 = 85.5
+# marks2 = 85.6
+# marks3 = 85.7
+# marks4 = 85.8
+# marks5 = 85.9
+
+marks = [34.3, 34.2, 64.3, 62.4, 80.3]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[1])
+
+
+student = ["Ashraf", 3232, "Dhaka", 62.4, 80.3]
+print(student)
+print(type(student))
+print(len(student))
+print(student[1])
+
+student[0] = "Bin Alam"
+print(student[0])
+
+# Slicing
+print(student[1:4]) #same of string slicing
